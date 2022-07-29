69d5510dd0e87384ef32fbd1a7c7c4f80efd4a71
Nitin Patil <105600797+nitinpatil219@users.noreply.github.com>
2022-07-29 15:06:04 +0530

    airbnb project

---
+++ src/Components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <img src="./gridlayout.png" alt="gridlayout" className="hero--photo" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
+export default Hero;
